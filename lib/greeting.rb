4a5a4bcd50f69e33844f15e17893d7b331287558
Brandon Hart <sugb73@gmail.com>
2020-12-02 16:05:32 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,5 +1,3 @@
-puts "Please enter your name so we can greet you more personally. "
-gets.strip
-puts "Hello #{name}. It's nice to meet you."
-greeting(name)
+def greeting(name)
+  puts "Hello #{name}. It's nice to meet you."
 end
